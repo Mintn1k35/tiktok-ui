04a2d3589ae1d345da2588bfbafefadc8de2958e
Phan Lê Min <mintn1k35@gmail.com>
2022-08-13 15:15:54 +0700

    Dung khung layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,14 @@
+// import classNames from 'classnames';
+import styles from './Header.module.scss';
+
+// const cx = classNames.bind(styles);
+
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <header className={styles.wrapper}>
+      <div className={styles.inner}></div>
+    </header>
+  );
 }
 
 export default Header;
